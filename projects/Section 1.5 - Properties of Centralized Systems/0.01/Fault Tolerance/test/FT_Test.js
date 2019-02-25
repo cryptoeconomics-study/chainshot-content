923cbdebab384fabf2b8f8d7afa96c975dcd7913
Kevin Ho <kevinjho1996@gmail.com>
2019-02-24 16:02:12 -0800

    1.5 fault tolerance test

---
+++ projects/Section 1.5 - Properties of Centralized Systems/0.01/Fault Tolerance/test/FT_Test.js
@@ -0,0 +1,10 @@
+import Paypal from '../Paypal.js';
+import Client from '../Client.js'
+const assert = require('assert');
+import EthCrypto from 'eth-crypto';
+
+describe('Fault Tolernce', function () {
+    it('should just pass', function () {
+        assert(true)
+    })
+})
